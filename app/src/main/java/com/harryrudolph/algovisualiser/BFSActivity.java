7b9bcad7f6a77ead5273588923ef981742c3ce90
Harry Rudolph <harryrudolph@me.com>
2021-02-25 14:59:29 +0000

    circles with numbers

--- app/src/main/java/com/harryrudolph/algovisualiser/BFSActivity.java
+++ app/src/main/java/com/harryrudolph/algovisualiser/BFSActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.harryrudolph.algovisualiser.views.GraphView;
+
 public class BFSActivity extends AppCompatActivity {
+
+    private GraphView mCustomView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b_f_s);
+
+        setTitle("Breadth First Search");
+
+
     }
 }
--- app/src/main/java/com/harryrudolph/algovisualiser/views/GraphView.java
+++ app/src/main/java/com/harryrudolph/algovisualiser/views/GraphView.java
@@ -13,31 +13,33 @@ import androidx.annotation.RequiresApi;
 
 import com.harryrudolph.algovisualiser.R;
 
-public class CustomView extends View {
+public class GraphView extends View {
 
     private static final int CIRCLESIZE = 75;
     private Paint graphColor;
+    private Paint textColor;
 
-    public CustomView(Context context) {
+
+    public GraphView(Context context) {
         super(context);
 
         init(null);
     }
 
-    public CustomView(Context context, @Nullable AttributeSet attrs) {
+    public GraphView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
         init(attrs);
     }
 
-    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public GraphView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
         init(attrs);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    public GraphView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
 
         init(attrs);
@@ -47,6 +49,15 @@ public class CustomView extends View {
         graphColor =  new Paint(Paint.ANTI_ALIAS_FLAG); //Ensure anti-aliasing on
         graphColor.setColor(Color.rgb(139,195,74));
 
+        textColor = new Paint(Paint.ANTI_ALIAS_FLAG);
+        textColor.setColor(Color.WHITE);
+        textColor.setTextAlign(Paint.Align.CENTER);
+        textColor.setTextSize(50);
+    }
+
+    public void swapColor() {
+        graphColor.setColor(Color.RED);
+        postInvalidate(); //update the onDraw
     }
 
     @Override
@@ -54,9 +65,15 @@ public class CustomView extends View {
         float cOffsetX = 140;
         float cOffsetY = 100;
 
+        float tOffsetY = cOffsetY + 10;
+
+        Integer counter = 0;
+
         for (int y = 0; y < 5; y++){
             for (int x = 0; x < 5; x++) {
                 canvas.drawCircle(cOffsetX + (200*x), cOffsetY + (200*y), CIRCLESIZE, graphColor);
+                canvas.drawText(counter.toString(), cOffsetX + (200*x), tOffsetY + (200*y), textColor);
+                counter++;
             }
         }
         //canvas.drawColor(Color.RED); //Background Colour
