3c0b69ece48d38193c0bd4af7b51c860093aebc9
Harry Rudolph <harryrudolph@me.com>
2021-02-25 11:05:03 +0000

    buttons can print

--- app/src/main/java/com/harryrudolph/algovisualiser/MainActivity.java
+++ app/src/main/java/com/harryrudolph/algovisualiser/MainActivity.java
@@ -3,12 +3,45 @@ package com.harryrudolph.algovisualiser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.mainButton1:
+                System.out.println("button1");
+                break;
+            case R.id.mainButton2:
+                System.out.println("button2");
+                break;
+            case R.id.mainButton3:
+                System.out.println("button3");
+                break;
+            default:
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mainButton1 = findViewById(R.id.mainButton1);
+        mainButton1.setOnClickListener(this);
+
+        Button mainButton2 = findViewById(R.id.mainButton2);
+        mainButton2.setOnClickListener(this);
+
+        Button mainButton3 = findViewById(R.id.mainButton3);
+        mainButton3.setOnClickListener(this);
+
+
     }
+
+
+
 }
