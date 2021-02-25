b928bb65d17580453524138ef9a7f5f62c2e68c4
Harry Rudolph <harryrudolph@me.com>
2021-02-25 14:59:17 +0000

    added todo

--- app/src/main/java/com/harryrudolph/algovisualiser/views/GraphView.java
+++ app/src/main/java/com/harryrudolph/algovisualiser/views/GraphView.java
@@ -3,6 +3,7 @@ package com.harryrudolph.algovisualiser.views;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
@@ -10,7 +11,12 @@ import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 
+import com.harryrudolph.algovisualiser.R;
+
 public class CustomView extends View {
+
+    private static final int CIRCLESIZE = 75;
+    private Paint graphColor;
 
     public CustomView(Context context) {
         super(context);
@@ -38,11 +44,21 @@ public class CustomView extends View {
     }
 
     private void init(@Nullable AttributeSet set){
+        graphColor =  new Paint(Paint.ANTI_ALIAS_FLAG); //Ensure anti-aliasing on
+        graphColor.setColor(Color.rgb(139,195,74));
 
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawColor(Color.RED);
+        float cOffsetX = 140;
+        float cOffsetY = 100;
+
+        for (int y = 0; y < 5; y++){
+            for (int x = 0; x < 5; x++) {
+                canvas.drawCircle(cOffsetX + (200*x), cOffsetY + (200*y), CIRCLESIZE, graphColor);
+            }
+        }
+        //canvas.drawColor(Color.RED); //Background Colour
     }
 }
