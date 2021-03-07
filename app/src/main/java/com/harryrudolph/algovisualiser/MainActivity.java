df8a88b5c4f1128a19ad3df3fe77d38539530992
Harry Rudolph <harryrudolph@me.com>
2021-03-07 15:56:31 +0000

    MainActivity.java commented

--- app/src/main/java/com/harryrudolph/algovisualiser/MainActivity.java
+++ app/src/main/java/com/harryrudolph/algovisualiser/MainActivity.java
@@ -8,12 +8,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toolbar;
 
+/**
+ * A class for the MainActivity. This is the first screen the user comes to.
+ * It implements an OnClickListener, waiting for the user to press one of three buttons.
+ */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    /**
+     * A method to wait for the user to press one of three buttons.
+     * @param v current view
+     */
     @Override
     public void onClick(View v) {
         Intent intent = new Intent(MainActivity.this, InfoActivity.class);
 
+        //Depending on ID pass different extra information to the Intent
         switch (v.getId()) {
             case R.id.mainBFSButton:
                 intent.putExtra("Algo", "BFS");
@@ -22,18 +31,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.mainDFSButton:
                 intent.putExtra("Algo", "DFS");
                 startActivity(intent);
-
                 break;
+
             case R.id.mainNQUEENSButton:
                 intent.putExtra("Algo", "NQUEENS");
                 startActivity(intent);
-
                 break;
+
             default:
                 break;
         }
     }
 
+    /**
+     * A method that is called when the activity is created. Sets onclick listener
+     * for each of the buttons.
+     * @param savedInstanceState null as no saved state is provided
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,10 +61,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button mainButton3 = findViewById(R.id.mainNQUEENSButton);
         mainButton3.setOnClickListener(this);
-
-
     }
-
-
 
 }
